1384bc9c187f0a31ebd8b6a284b991e8bc11605b
Marco Carcini <marco.carcini@gmail.com>
2019-09-06 14:59:24 -0500

    Add model restaurant

---
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData() {
+            restaurants = new List<Restaurant>
+            {
+                new Restaurant{ Id=1, Name = "Pizza Italiana", Cuisine=CuisineType.Italian},
+                new Restaurant{ Id=2, Name = "Tersiguels", Cuisine=CuisineType.French},
+                new Restaurant{ Id=3, Name = "Mango Grove", Cuisine=CuisineType.Indian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
