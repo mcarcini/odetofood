9d9089e15f8944216f142be7d02fb8485b6c92a6
Marco Carcini <marco.carcini@gmail.com>
2019-09-24 23:50:38 -0500

    create a data access service

---
+++ OdeToFood/OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext dbContext;
+
+        public SqlRestaurantData(OdeToFoodDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            dbContext.Restaurants.Add(restaurant);
+            dbContext.SaveChanges();
+        }
+
+        public void Edit(Restaurant restaurant)
+        {
+            var entry = dbContext.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return dbContext.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            //return dbContext.Restaurants;
+            return from r in dbContext.Restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
